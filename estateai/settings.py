429c7a04319ae8d5ea6bc1b4f5e1c73e9bf63f09
Rupesh Rana <rupeshrana20274@gmail.com>
2026-09-10 18:22:41 +0530

    add cores middleware

--- estateai/settings.py
+++ estateai/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
+    'corsheaders',
 
     'apps.properties',
     "apps.chatbot",
@@ -63,6 +64,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -70,6 +72,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True   # fine for a local demo; tighten later
 
 ROOT_URLCONF = "estateai.urls"
 
